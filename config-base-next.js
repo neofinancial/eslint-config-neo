38285fc91c8c965e3f7cbaadf256c6a3f8d0a2b0
Aries Dimaranan <aasdimaranan@gmail.com>
2023-03-07 17:44:53 -0700

    removed disabled rules in base-next config

--- config-base-next.js
+++ config-base-next.js
@@ -23,12 +23,6 @@ module.exports = {
     },
     {
       files: ['**/test/**/*'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-empty-function': 'off',
-        '@typescript-eslint/require-await': 'off',
-      },
     },
   ],
 };

4ad2dd771da858944c02ce440fab91b5c99a0a01
Leah Copeland <leah.copeland@neofinancial.com>
2025-04-24 08:23:16 -0600

    add some linting rules to base and organize by off warn and error

--- config-base.js
+++ config-base.js
@@ -8,10 +8,11 @@ module.exports = {
     'plugin:unicorn/recommended',
     'plugin:promise/recommended',
     'prettier',
+    'plugin:you-dont-need-lodash-underscore/compatible',
   ],
   plugins: ['node', 'jest', 'unicorn', 'promise', 'import', 'autofix'],
   parserOptions: {
-    ecmaVersion: 2021,
+    ecmaVersion: 2022,
     sourceType: 'module',
     project: true,
   },
@@ -26,12 +27,16 @@ module.exports = {
       plugins: ['@typescript-eslint'],
       parser: '@typescript-eslint/parser',
       parserOptions: {
-        ecmaVersion: 2021,
+        ecmaVersion: 2022,
         sourceType: 'module',
         project: true,
         warnOnUnsupportedTypeScriptVersion: true,
       },
-      // if adding a typescript-eslint version of an existing ESLint rule make sure to disable the ESLint rule here
+      /**
+       * Global overrides.
+       * if adding a typescript-eslint version of an existing ESLint rule,
+       * make sure to disable the ESLint rule here
+       */
       rules: {
         // 'tsc' already handles this: https://github.com/typescript-eslint/typescript-eslint/issues/291
         'no-dupe-class-members': 'off',
@@ -39,6 +44,13 @@ module.exports = {
         'no-undef': 'off',
         'no-use-before-define': 'off',
         'no-array-constructor': 'off',
+        'unicorn/no-null': 'off',
+        'unicorn/no-useless-undefined': 'off',
+
+        '@typescript-eslint/no-array-constructor': 'warn',
+        '@typescript-eslint/return-await': ['warn', 'always'],
+        '@typescript-eslint/prefer-optional-chain': 'warn',
+        '@typescript-eslint/strict-boolean-expressions': 'warn',
 
         //TODO: change to { assertionStyle: 'never' } when config-dug supports primitive types
         '@typescript-eslint/consistent-type-assertions': [
@@ -46,18 +58,17 @@ module.exports = {
           { assertionStyle: 'as', objectLiteralTypeAssertions: 'never' },
         ],
         '@typescript-eslint/explicit-function-return-type': 'error',
-        '@typescript-eslint/no-array-constructor': 'warn',
         '@typescript-eslint/no-floating-promises': ['error', { ignoreIIFE: true }],
+        '@typescript-eslint/no-misused-promises': 'error',
         '@typescript-eslint/no-namespace': 'error',
         '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_+', varsIgnorePattern: '^_+' }],
-        '@typescript-eslint/return-await': ['warn', 'always'],
         'autofix/no-unused-vars': ['error', { argsIgnorePattern: '^_+', varsIgnorePattern: '^_+' }],
         '@typescript-eslint/no-use-before-define': ['error', { typedefs: false, enums: false }],
-        '@typescript-eslint/prefer-optional-chain': 'warn',
-        'unicorn/no-null': 'off',
-        'unicorn/no-useless-undefined': 'off',
       },
     },
+    /**
+     * Test overrides.
+     */
     {
       files: ['**/test/**/*'],
       rules: {
@@ -71,76 +82,20 @@ module.exports = {
     },
   ],
   rules: {
-    curly: 'error',
-    eqeqeq: ['error', 'smart'],
-    'func-names': ['warn', 'as-needed'],
-    'jest/prefer-to-be': 'off',
-    'no-bitwise': 'warn',
-    'no-console': 'warn',
-    'no-default-export': 'off',
-    'no-duplicate-imports': 'error',
-    'no-eval': 'error',
-    'no-extra-bind': 'warn',
-    'no-lonely-if': 'warn',
-    'no-loop-func': 'error',
-    'no-mixed-requires': 'error',
-    'no-new-require': 'error',
-    'no-param-reassign': 'error',
+    // OFF
     'no-process-exit': 'off',
-    'no-restricted-imports': 'off', // use @typescript-eslint/no-restricted-imports instead
-    'no-return-await': 'error',
-    'no-throw-literal': 'error',
-    'no-undef-init': 'error',
-    'no-unneeded-ternary': 'warn',
-    'no-use-before-define': ['error', 'nofunc'],
-    'no-useless-concat': 'warn',
-    'no-var': 'warn',
-    'padding-line-between-statements': [
-      'warn',
-      { blankLine: 'always', prev: '*', next: 'return' },
-      { blankLine: 'always', prev: '*', next: 'throw' },
-      { blankLine: 'always', prev: '*', next: 'block-like' },
-      { blankLine: 'always', prev: 'block-like', next: '*' },
-      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
-      { blankLine: 'always', prev: '*', next: ['const', 'let', 'var'] },
-      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
-    ],
-    'prefer-const': 'warn',
-    'prefer-template': 'warn',
-    // disable atomic updates rule until this issue is resolved: https://github.com/eslint/eslint/issues/11899
-    'require-atomic-updates': 'off',
-    'require-await': 'off', // use @typescript-eslint/no-floating-promises instead
-
-    'node/exports-style': 'error',
-    'node/no-unpublished-import': 'off',
+    'jest/prefer-to-be': 'off',
+    'no-default-export': 'off',
+    // use @typescript-eslint/no-restricted-imports instead
+    'no-restricted-imports': 'off',
     'node/no-missing-import': 'off',
-    'node/no-unsupported-features/es-syntax': [
-      'warn',
-      {
-        ignores: ['modules'],
-      },
-    ],
+    'node/no-unpublished-import': 'off',
     'unicorn/catch-error-name': 'off',
     'unicorn/consistent-function-scoping': 'off',
-    'unicorn/expiring-todo-comments': [
-      'warn',
-      {
-        terms: ['todo'],
-      },
-    ],
     'unicorn/no-abusive-eslint-disable': 'off',
     'unicorn/no-array-for-each': 'off',
     'unicorn/no-await-expression-member': 'off',
     'unicorn/no-fn-reference-in-iterator': 'off',
-    'unicorn/no-nested-ternary': 'warn',
-    'unicorn/no-null': 'off',
-    'unicorn/no-process-exit': 'off',
-    'unicorn/no-reduce': 'warn',
-    'unicorn/numeric-separators-style': 'off',
-    'unicorn/prefer-includes': 'off',
-    'unicorn/prefer-module': 'off',
-    'unicorn/prefer-node-protocol': 'off',
-    'unicorn/prefer-optional-catch-binding': 'warn',
     'unicorn/prefer-set-has': 'off',
     'unicorn/prefer-ternary': 'off',
     'unicorn/prefer-top-level-await': 'off',
@@ -152,6 +107,76 @@ module.exports = {
     'unicorn/prefer-spread': 'off',
     'unicorn/prefer-export-from': 'off',
     'unicorn/prefer-switch': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/no-process-exit': 'off',
+    'unicorn/numeric-separators-style': 'off',
+    'unicorn/prefer-includes': 'off',
+    'unicorn/prefer-module': 'off',
+    'unicorn/prefer-node-protocol': 'off',
+
+    // WARN
+    'no-bitwise': 'warn',
+    'no-console': 'warn',
+    'no-lonely-if': 'warn',
+    'prefer-const': 'warn',
+    'no-extra-bind': 'warn',
+    'prefer-template': 'warn',
+    'no-useless-concat': 'warn',
+    'no-unneeded-ternary': 'warn',
+    'func-names': ['warn', 'as-needed'],
+    'no-var': 'warn',
+    'unicorn/expiring-todo-comments': [
+      'warn',
+      {
+        terms: ['todo'],
+      },
+    ],
+    'padding-line-between-statements': [
+      'warn',
+      { blankLine: 'always', prev: '*', next: 'return' },
+      { blankLine: 'always', prev: '*', next: 'throw' },
+      { blankLine: 'always', prev: '*', next: 'block-like' },
+      { blankLine: 'always', prev: 'block-like', next: '*' },
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      { blankLine: 'always', prev: '*', next: ['const', 'let', 'var'] },
+      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] },
+    ],
+    'node/no-unsupported-features/es-syntax': [
+      'warn',
+      {
+        ignores: ['modules'],
+      },
+    ],
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          ['builtin', 'external'],
+          ['sibling', 'parent'],
+        ],
+        'newlines-between': 'always-and-inside-groups',
+      },
+    ],
+    'import/newline-after-import': 'warn',
+    'autofix/sort-imports': ['warn', { ignoreCase: true, ignoreDeclarationSort: true }],
+    'unicorn/prefer-optional-catch-binding': 'warn',
+    'unicorn/no-nested-ternary': 'warn',
+    'unicorn/no-reduce': 'warn',
+
+    // ERROR
+    curly: 'error',
+    'no-eval': 'error',
+    'no-loop-func': 'error',
+    'no-new-require': 'error',
+    'no-return-await': 'error',
+    'no-throw-literal': 'error',
+    eqeqeq: ['error', 'smart'],
+    'no-mixed-requires': 'error',
+    'no-param-reassign': 'error',
+    'no-duplicate-imports': 'error',
+    'no-undef-init': 'error',
+    'no-use-before-define': ['error', 'nofunc'],
+    'node/exports-style': 'error',
     'import/no-restricted-paths': [
       'error',
       {
@@ -168,17 +193,5 @@ module.exports = {
         peerDependencies: false,
       },
     ],
-    'import/order': [
-      'warn',
-      {
-        groups: [
-          ['builtin', 'external'],
-          ['sibling', 'parent'],
-        ],
-        'newlines-between': 'always-and-inside-groups',
-      },
-    ],
-    'import/newline-after-import': 'warn',
-    'autofix/sort-imports': ['warn', { ignoreCase: true, ignoreDeclarationSort: true }],
   },
 };
